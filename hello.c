4e82c6fd40915b43733a8f5a83980e43c604f4f4
Wes Dennison <wdennison@huskers.unl.edu>
2020-08-23 21:00:26 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Wes Dennison
+ *         wdennison2@huskers.unl.edu
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Wes Dennison!\n");
+
+  return 0;
+}
